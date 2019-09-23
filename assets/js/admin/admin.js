6044181c162a5fa9044aa7a6c6f2afe8ce161a12
Ned Zimmerman <ned@bight.ca>
2019-09-23 11:43:46 -0600

    Add archival link fields (resolves #6, resolves #54, resolves #72) (#84)
    * Add initial fields for Perma.cc and the Wayback Machine
    
    * Use pll__ for strings
    
    * Support multiple links for both Perma.cc and Wayback
    
    * Add validation for Perma.cc and Wayback Machine URL domains
    
    * Adjust localize utility to ignore PHP files (resolve #54)
    
    * Ensure that archival link is a valid URL before checking domain
    
    * Accept http or https protocols for archival links
    
    * Ensure that entured URLs have a valid scheme
    
    * Only highlight the field with an error in a repeatable group
    Catch URLs without a scheme on form submission
    
    * Don't add scheme to blank URL

--- assets/js/admin/admin.js
+++ assets/js/admin/admin.js
@@ -1,11 +1,48 @@
 const { __, sprintf } = wp.i18n;
+const schemify = require( 'url-schemify' );
 
 jQuery( document ).ready( function( $ ) {
 	const $form = $( '#post' );
+	const $urlFields = $( '.cmb2-text-url' );
 	const $toValidate = $( '[data-validation]' );
+
+	$urlFields.blur( ( e ) => {
+		const val = $( e.target ).val();
+		if ( 0 !== val.length ) {
+			$( e.target ).val( schemify( val ) );
+		}
+	} );
 
 	if ( !$toValidate.length ) {
 		return;
+	}
+
+	/**
+	 * Ensure that a user-supplied URL is a valid URL.
+	 *
+	 * @param {string} value The URL that the user has entered.
+	 */
+	function isUrl( value ) {
+		try {
+			new URL( value );
+			return true;
+		} catch ( e ) {
+			return false;
+		}
+	}
+
+	/**
+	 * Ensure that a user-supplied URL matches an expected domain.
+	 *
+	 * @param {string} expectedDomain The expected domain of the URL.
+	 * @param {string} actualUrl The actual URL that the user has entered.
+	 */
+	function checkUrlDomain( expectedDomain, actualUrl ) {
+		const actualDomain = new URL( actualUrl ).hostname;
+		if ( actualDomain === expectedDomain ) {
+			return true;
+		}
+		return false;
 	}
 
 	/**
@@ -18,6 +55,15 @@ jQuery( document ).ready( function( $ ) {
 		let $firstError = null;
 		$( '.error' ).remove();
 
+		$urlFields.each( ( i, e ) => {
+			if ( $( e ).is( ':visible' ) ) {
+				const val = $( e ).val();
+				if ( 0 !== val.length ) {
+					$( e ).val( schemify( val ) );
+				}
+			}
+		} );
+
 		/**
 		 * Validate a form element.
 		 *
@@ -27,13 +73,28 @@ jQuery( document ).ready( function( $ ) {
 			const $this = $( element );
 			const val = $this.val();
 			const $row = $this.parents( '.cmb-row' );
+			let valid = false;
 
-			if ( 'required' === $this.data( 'validation' ) ) {
+			if ( 'true' === $this.data( 'required' ) ) {
 				if ( ! val ) {
-					addRequired( $row );
+					addRequiredError( $row );
+					valid = false;
 				} else {
-					removeRequired( $row );
+					valid = true;
 				}
+			}
+
+			if ( $this.data( 'domain' ) && $this.is( ':visible' ) ) {
+				if ( 0 !== val.length && ( ! isUrl( val ) || ! checkUrlDomain( $this.data( 'domain' ), val ) ) ) {
+					addDomainMismatchError( $row, $this, $this.data( 'domain' ) );
+					valid = false;
+				} else {
+					valid = true;
+				}
+			}
+
+			if ( valid ) {
+				removeError( $row );
 			}
 		}
 
@@ -42,25 +103,51 @@ jQuery( document ).ready( function( $ ) {
 		 *
 		 * @param {jQuery} $row
 		 */
-		function addRequired( $row ) {
+		function addRequiredError( $row ) {
 			const $label = $row.find( '.cmb-th label' );
 			$errorFields.push(
-				{ id: $label.attr( 'for' ), label: $label.text() }
+				{ id: $label.attr( 'for' ), label: $label.text(), type: 'required' }
 			);
 			$row.addClass( 'form-invalid' );
+			/* translators: %s: The label of the required field. */
 			const errorText = sprintf( __( 'A %s is required.', 'learning-commons-framework' ), $label.text().toLowerCase() );
 			const error = $( `<p class="error">${errorText}</p>` );
 			$row.children( '.cmb-td' ).append( error );
+			$firstError = $firstError ? $firstError : $row;
+		}
+
+		/**
+		 * Add domain mismatch flag to a form field.
+		 *
+		 * @param {jQuery} $row
+		 * @param {jQuery} $field
+		 * @param {string} expectedDomain
+		 */
+		function addDomainMismatchError( $row, $field, expectedDomain ) {
+			const $label = $row.find( '.cmb-th label' );
+
+			$errorFields.push(
+				{ id: $row.hasClass( 'cmb-repeat' ) ? `${$label.attr( 'for' )}_repeat` : $label.attr( 'for' ), label: $label.text(), type: 'domain', expected: expectedDomain }
+			);
+			if ( $row.hasClass( 'cmb-repeat' ) ) {
+				$field.parent( '.cmb-td' ).parent( '.cmb-repeat-row' ).addClass( 'form-invalid' );
+			} else {
+				$row.addClass( 'form-invalid' );
+			}
+			/* translators: %s: The expected domain name for the URL input. */
+			const errorText = sprintf( __( 'The URL must be an address at the domain <em>%s</em>.', 'learning-commons-framework' ), expectedDomain );
+			const error = $( `<p class="error">${errorText}</p>` );
+			$field.parent( '.cmb-td' ).append( error );
 			$firstError = $firstError ? $firstError : $row;
 
 		}
 
 		/**
-		 * Remove required flag from a form row.
+		 * Remove error class from a form row.
 		 *
 		 * @param {jQuery} $row
 		 */
-		function removeRequired( $row ) {
+		function removeError( $row ) {
 			$row.removeClass( 'form-invalid' );
 		}
 
@@ -73,7 +160,15 @@ jQuery( document ).ready( function( $ ) {
 			$( '#validation-message' ).remove();
 			const errorMessage = __( 'The form contains errors:', 'learning-commons-framework' );
 			const errorList = $errorFields.reduce( ( html, field, index ) => {
-				const errorText = sprintf( __( 'A %s is required.', 'learning-commons-framework' ), field.label.toLowerCase() );
+				let errorText;
+				if ( 'required' === field.type ) {
+					/* translators: %s: The label of the required field. */
+					errorText = sprintf( __( 'A %s is required.', 'learning-commons-framework' ), field.label.toLowerCase() );
+				}
+				if ( 'domain' == field.type ) {
+					/* translators: %s: The expected domain name for the URL field. */
+					errorText = sprintf( __( 'The URL must be an address at the domain <em>%s</em>.', 'learning-commons-framework' ), field.expected );
+				}
 				if ( 0 < index ) {
 					return `${html}<li><a href="#${field.id}">${errorText}</a></li>`;
 				} else {
--- includes/functions/metadata.php
+++ includes/functions/metadata.php
@@ -42,6 +42,32 @@ function register_meta() {
 
 	register_post_meta(
 		'lc_resource',
+		'lc_resource_perma_cc_links',
+		[
+			'type'         => 'string',
+			'description'  => 'A link or links to an archival copy of the resource on Perma.cc.',
+			'single'       => true,
+			'show_in_rest' => [
+				'prepare_callback' => 'LearningCommonsFramework\Metadata\prepare_archival_links',
+			],
+		]
+	);
+
+	register_post_meta(
+		'lc_resource',
+		'lc_resource_wayback_machine_links',
+		[
+			'type'         => 'string',
+			'description'  => 'A link or links to an archival copy of the resource on the Wayback Machine.',
+			'single'       => true,
+			'show_in_rest' => [
+				'prepare_callback' => 'LearningCommonsFramework\Metadata\prepare_archival_links',
+			],
+		]
+	);
+
+	register_post_meta(
+		'lc_resource',
 		'lc_resource_author',
 		[
 			'type'         => 'string',
@@ -168,6 +194,24 @@ function prepare_contributors( $value ) {
 }
 
 /**
+ * Prepare `lc_resource_perma_cc_links` and `lc_resource_wayback_machine_links`
+ * for REST API access.
+ *
+ * @param mixed $value The metadata value.
+ *
+ * @return array
+ */
+function prepare_archival_links( $value ) {
+	$result = [];
+	if ( is_array( $value ) ) {
+		foreach ( $value as $v ) {
+			$result[] = $v;
+		}
+	}
+	return $result;
+}
+
+/**
  * Prepare `lc_resource_revisions` for REST API access.
  *
  * @param mixed $value The metadata value.
@@ -193,151 +237,193 @@ function resource_data_init() {
 	$prefix = 'lc_resource_';
 
 	$cmb = new_cmb2_box(
-		array(
+		[
 			'id'           => 'resource_data',
 			'title'        => pll__( 'Resource Data' ),
-			'object_types' => array( 'lc_resource' ),
+			'object_types' => [ 'lc_resource' ],
 			'context'      => 'normal',
 			'priority'     => 'high',
 			'show_names'   => true,
-		)
+		]
 	);
 
 	$cmb->add_field(
-		array(
+		[
 			'name'        => pll__( 'Permanent Link' ),
 			'description' => pll__( 'A permanent link to the resource.' ),
 			'id'          => $prefix . 'permanent_link',
 			'type'        => 'text_url',
-			'protocols'   => array( 'http', 'https' ),
+			'protocols'   => [ 'http', 'https' ],
 			'attributes'  => [
-				'data-validation' => 'required',
+				'data-validation' => 'true',
+				'data-required'   => 'true',
 			],
-		)
+		]
+	);
+
+	// TODO: Validate that the URL starts with https://perma.cc/
+	$cmb->add_field(
+		[
+			'name'        => pll__( 'Perma.cc Link', 'learning-commons-framework' ),
+			'description' => pll__( 'A link or links to an archival copy of the resource on <a href="https://perma.cc">Perma.cc</a>. If the resource spans multiple pages on Perma.cc, you may add multiple links.', 'learning-commons-framework' ),
+			'id'          => $prefix . 'perma_cc_links',
+			'type'        => 'text_url',
+			'repeatable'  => true,
+			'protocols'   => [ 'http', 'https' ],
+			'text'        => [
+				'add_row_text' => pll__( 'Add Link' ),
+			],
+			'attributes'  => [
+				'aria-label'      => pll__( 'Perma.cc Link' ),
+				'data-validation' => 'true',
+				'data-domain'     => 'perma.cc',
+			],
+		]
+	);
+
+	// TODO: Validate that the URL starts with https://web.archive.org/
+	$cmb->add_field(
+		[
+			'name'        => pll__( 'Wayback Machine Link', 'learning-commons-framework' ),
+			'description' => pll__( 'A link or links to an archival copy of the resource on the <a href="https://web.archive.org">Wayback Machine</a>. If the resource spans multiple pages on the Wayback Machine, you may add multiple links.', 'learning-commons-framework' ),
+			'id'          => $prefix . 'wayback_machine_links',
+			'type'        => 'text_url',
+			'repeatable'  => true,
+			'protocols'   => [ 'http', 'https' ],
+			'text'        => [
+				'add_row_text' => pll__( 'Add Link' ),
+			],
+			'attributes'  => [
+				'aria-label'      => pll__( 'Wayback Machine Link' ),
+				'data-validation' => 'true',
+				'data-domain'     => 'web.archive.org',
+			],
+		]
 	);
 
 	// TODO: Don't save any authors if they are empty.
 	$cmb->add_field(
-		array(
+		[
 			'name'        => pll__( 'Author' ),
 			'description' => pll__( 'The author of the resource.' ),
 			'id'          => $prefix . 'author',
 			'type'        => 'text',
 			'repeatable'  => true,
-			'text'        => array(
+			'text'        => [
 				'add_row_text' => pll__( 'Add Author' ),
-			),
+			],
 			'attributes'  => [
 				'aria-label' => pll__( 'Author' ),
 			],
-		)
+		]
 	);
 
 	// TODO: Don't save any authors if they are empty.
 	$cmb->add_field(
-		array(
+		[
 			'name'        => pll__( 'Editor' ),
 			'description' => pll__( 'The editor of the resource.' ),
 			'id'          => $prefix . 'editor',
 			'type'        => 'text',
 			'repeatable'  => true,
-			'text'        => array(
+			'text'        => [
 				'add_row_text' => pll__( 'Add Editor' ),
-			),
+			],
 			'attributes'  => [
 				'aria-label' => pll__( 'Editor' ),
 			],
-		)
+		]
 	);
 
 	// TODO: Don't save any translators if they are empty.
 	$cmb->add_field(
-		array(
+		[
 			'name'        => pll__( 'Translator' ),
 			'description' => pll__( 'The translator of the resource.' ),
 			'id'          => $prefix . 'translator',
 			'type'        => 'text',
 			'repeatable'  => true,
-			'text'        => array(
+			'text'        => [
 				'add_row_text' => pll__( 'Add Translator' ),
-			),
+			],
 			'attributes'  => [
 				'aria-label' => pll__( 'Translator' ),
 			],
-		)
+		]
 	);
 
 	$cmb->add_field(
-		array(
+		[
 			'name'        => pll__( 'Publication Date' ),
 			'description' => pll__( 'The publication date of the resource in YYYY-MM-DD format.' ),
 			'id'          => $prefix . 'publication_date',
 			'type'        => 'text_date',
 			'date_format' => 'Y-m-d',
 			'attributes'  => [
-				'data-validation' => 'required',
+				'data-validation' => 'true',
+				'data-required'   => 'true',
 			],
-		)
+		]
 	);
 
 	// TODO: Don't save any revisions if they are empty.
 	$group_field_id = $cmb->add_field(
-		array(
+		[
 			'id'          => $prefix . 'revisions',
 			'type'        => 'group',
 			'description' => pll__( 'Revisions of the resource.' ),
-			'options'     => array(
+			'options'     => [
 				'group_title'    => pll__( 'Revision {#}' ),
 				'add_button'     => pll__( 'Add Revision' ),
 				'remove_button'  => pll__( 'Remove Revision' ),
 				'sortable'       => true,
 				'closed'         => true,
 				'remove_confirm' => pll__( 'Are you sure you want to remove this revision?' ),
-			),
-		)
+			],
+		]
 	);
 
 	$cmb->add_group_field(
 		$group_field_id,
-		array(
+		[
 			'name'        => pll__( 'Revision Date' ),
 			'description' => pll__( 'The date of this revision in YYYY-MM-DD format.' ),
 			'id'          => $prefix . 'revision_date',
 			'type'        => 'text_date',
 			'date_format' => 'Y-m-d',
-		)
+		]
 	);
 
 	$cmb->add_group_field(
 		$group_field_id,
-		array(
+		[
 			'name'        => pll__( 'Revision Description' ),
 			'description' => pll__( 'A brief description of this revision.' ),
 			'id'          => $prefix . 'revision_description',
 			'type'        => 'textarea_small',
-		)
+		]
 	);
 
 	$cmb->add_field(
-		array(
+		[
 			'name'        => pll__( 'Publisher Name' ),
 			'description' => pll__( 'The publisher of the resource.' ),
 			'id'          => $prefix . 'publisher_name',
 			'type'        => 'text',
-		)
+		]
 	);
 
 	$cmb->add_field(
-		array(
+		[
 			'name'        => pll__( 'Publisher City' ),
 			'description' => pll__( 'The town or city where the publisher of the resource is located.' ),
 			'id'          => $prefix . 'publisher_locality',
 			'type'        => 'text',
-		)
+		]
 	);
 
 	$cmb->add_field(
-		array(
+		[
 			'name'             => pll__( 'Publisher Country' ),
 			'description'      => pll__( 'The country where the publisher of the resource is located.' ),
 			'id'               => $prefix . 'publisher_country',
@@ -345,16 +431,16 @@ function resource_data_init() {
 			'show_option_none' => true,
 			'default'          => '',
 			'options'          => \LearningCommonsFramework\Internationalization\get_country_list( get_user_locale() ),
-		)
+		]
 	);
 
 	$cmb->add_field(
-		array(
+		[
 			'name'        => pll__( 'Publisher Link' ),
 			'description' => pll__( 'A link to the publisher of the resource.' ),
 			'id'          => $prefix . 'publisher_link',
 			'type'        => 'text_url',
-			'protocols'   => array( 'http', 'https' ),
-		)
+			'protocols'   => [ 'http', 'https' ],
+		]
 	);
 }
