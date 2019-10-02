b23c224bac401a63c1a6321b87ef83619f6e78e5
Ned Zimmerman <ned@bight.ca>
2019-10-02 13:29:27 -0600

    Customize columns in Resource list (#120)
    * Customize columns in Resource list
    
    * Add topics column

--- includes/functions/core.php
+++ includes/functions/core.php
@@ -72,6 +72,25 @@ function resource_init() {
 	register_extended_post_type(
 		'lc_resource',
 		array(
+			'admin_cols'          => [
+				'title',
+				'language'  => [
+					'title'    => __( 'Language', 'learning-commons-framework' ),
+					'taxonomy' => 'language',
+				],
+				'format'    => [
+					'title'    => __( 'Format', 'learning-commons-framework' ),
+					'taxonomy' => 'lc_format',
+				],
+				'topic'    => [
+					'title'    => __( 'Topics', 'learning-commons-framework' ),
+					'taxonomy' => 'lc_topic',
+				],
+				'published' => [
+					'title'    => 'Published',
+					'meta_key' => 'lc_resource_publication_year',
+				],
+			],
 			'labels'              => [
 				'name'                  => __( 'Resources', 'learning-commons-framework' ),
 				'singular_name'         => __( 'Resource', 'learning-commons-framework' ),
